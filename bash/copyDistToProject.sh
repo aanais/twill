c01b3d11e529fec5554a36b1a43256e3c9b76f74
Martin SMEECKAERT <martin@smithandjosette.com>
2021-08-12 10:07:04 +0200

    update bash script

--- bash/copyDistToProject.sh
+++ bash/copyDistToProject.sh
@@ -1,2 +1,4 @@
 cd ../../
-docker exec `docker container ls | grep fpm | awk '{print $NF}'` php artisan twill:update
+result=${PWD##*/}
+echo $result
+docker exec `docker container ls | grep fpm | awk '{print $NF}'` sh -c "cd $result && php artisan twill:update"
